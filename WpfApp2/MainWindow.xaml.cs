ace5dd625a75c75d7944f79451c3dd945b46e5af
is13126 <is13126@B206-2>
2023-04-29 08:45:50 +0700

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<product> products;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new List<product>()
+            {
+                new product("Рыба",10000),
+                new product("Фотопарат",10000),
+                new product("Ноутбук",30000),
+                new product("Видеокарта",55000)
+            };
+            update();
+        }
+        public void update()
+        {
+            
+            list1.ItemsSource = products.ToList();
+        }
+
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list1.ItemsSource = products.ToList();
+        }
+
+        private void tx1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var product1 = products.Where(u => u.Name(tx1.Text)).ToList();
+            list1.ItemsSource = product1;
+        }
+    }
+}
---
+++ WpfApp2/product.cs
@@ -0,0 +1,14 @@
+﻿namespace WpfApp2
+{
+    public class product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
